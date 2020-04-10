76638ed27509194539595828880c0fe94a0d876c
caroe2014 <edwincaro10@hotmail.com>
2020-04-10 10:42:33 -0500

    Initial deployment to Azure

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+import requests as rq
+import azure.storage.blob
+import os
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World from Flask with several libraries!"
+
+if __name__ == '__main__':
+    port= os.environ.get('PORT')
+    app.run(host='0.0.0.0', debug=False, port=port)
